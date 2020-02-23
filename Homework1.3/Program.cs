146c7846b611997ef5f713a0771a55a1527ec307
Nazar Borodin <Naz.borodin@gmail.com>
2020-02-23 09:49:13 +0200

    master firstapp in git

---
+++ Homework1.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter A: ");
+            str = Console.ReadLine();
+            int a = Convert.ToInt32(str);
+            Console.WriteLine("Enter B: ");
+            str = Console.ReadLine();
+            int b = Convert.ToInt32(str);
+            if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.WriteLine("{0}",i);
+                    }
+                    //Console.WriteLine("\n");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("a>b");
+            }
+            Console.ReadLine();
+        }
+    }
+}
